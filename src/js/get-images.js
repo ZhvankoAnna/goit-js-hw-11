deb35f79658d65eeb76ef5ab6950060f1e55060e
Zhvanko Anna <zhvankoann@gmail.com>
2023-01-24 23:51:12 +0200

    return axios.get params

--- src/js/get-images.js
+++ src/js/get-images.js
@@ -6,16 +6,16 @@ const API_KEY = '32975717-e5ee65230820405f183c875ea';
 
 export async function getImages(searchReq, pc) {
   try {
-    // const params = {
-    //   key: `${API_KEY}`,
-    //   q: `${searchReq}`,
-    //   image_type: 'photo',
-    //   orientation: 'horizontal',
-    //   safesearch: true,
-    //   per_page: 40,
-    //   page: `${pc}`,
-    // };
-    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchReq}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pc}`);
+    const params = {
+      key: `${API_KEY}`,
+      q: `${searchReq}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: `${pc}`,
+    };
+    const response = await axios.get(`${BASE_URL}`, {params});
     const data = await response.data;
     return data;
   } catch (error) {
